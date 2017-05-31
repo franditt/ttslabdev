52a7515734fd59e1a16dece8e2d567a33c435a27
jkleczar <jkleczar@gmail.com>
2017-05-31 16:45:43 +0200

    removed twi

--- voices/twi/make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ttslab_make_phoneset.py twi_default TwiPhoneset
-ttslab_make_g2p.py
-ttslab_make_pronundicts.py
-ttslab_make_voice.py frontend
-ttslab_make_voice.py wordusfrontend
-ttslab_make_voice.py wordus
